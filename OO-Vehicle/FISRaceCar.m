ef6bdc748c1044cf0f6263fd7d9f63b18249fc34
AmitaiB <blickstein@gmail.com>
2015-06-10 11:11:22 -0400

    fixes 2nd spelling error ('Pizza Hit'), passes all tests

--- OO-Vehicle/FISRaceCar.m
+++ OO-Vehicle/FISRaceCar.m
@@ -13,7 +13,7 @@
 //weight 1270, topSpeed 615, currentspeed and direction 0 - list of sponsors KFC, Taco Bell, and Pizza Hut
 -(instancetype)init {
     self = [self initWithWeight:1270 topSpeed:615 currentSpeed:0 currentDirection:0];
-    _sponsors = @[@"KFC", @"Taco Bell", @"Pizza Hit"];
+    _sponsors = @[@"KFC", @"Taco Bell", @"Pizza Hut"];
     return self;
 }
 
